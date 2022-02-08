c52e1d34dc3650295b269bcc9fa5ce5af12d8695
pallav <88773556+Pallav2905-py@users.noreply.github.com>
2022-02-08 23:20:08 +0530

    Started with Database and Sorting System

--- functions.py
+++ functions.py
@@ -2,17 +2,30 @@ from termcolor import colored, cprint
 
 def topbar():
     print("Welcome To Hotel Management System")
-    text = colored('Release-Alpha 0.0.1  "Designed and Maintained by TeamPallav@"', 'red', attrs=['blink','bold','dark'])
-    print(text)
-
+    cprint('Release-Alpha 0.0.1  "Designed and Maintained by TeamPallav@"', 'red', attrs=['blink','bold','dark'])
 def home():
     print("!!Shree!!")
     print("")
-    print("Select task from the following list:-")
+    cprint("Select task from the following list:-","yellow", attrs=["bold","dark",])
     print("")
     print("1]To Create a new user, Press 1")
-    print("2]To Create a new admin Press 2")
+    print("2]To Create a new admin, Press 2")
     print("3]To New Check-In press 3")
     print("")
     print("")
     print("More comming soon.....")
+    print("Press 'Ctrl+C' to cancel/crash the programm and exit at any time")
+def new_user():
+    print("Creating New User......")
+    username = input("Enter Your Name:- ")
+    userverificationtype = input("Enter Verification ID type:- ")
+    verifyid = input("Enter Verification ID Number:- ")
+    amount =  input("Enter The Amount For Bill:- ")
+    f = open("this.odf", "w")
+    f.write("User Name:-"+username+"\n")
+    f.write("User Verification type"+userverificationtype+"\n")
+    f.write("User Verification ID"+verifyid+"\n")
+    f.write("User Bill Amount Calculated As Per Reciptant"+amount+"\n")
+    f.close()
+
+    
--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 from functions import *
 topbar()
 home()
-hometask = input("Enter Your Required Test:- ")
+hometask = input(":-")
+
+if "1" in hometask:
+    new_user()
+elif "2" in hometask:
+    print("")
+elif "3" in hometask:
+    print("")
+elif "find" in hometask:
+    file = open("this.odf", "r")     #Opens the file in r mode
+    for lines in file:
+        if "pallav" in lines:
+            print("True"+lines)
+        else:
+            print("False")
+    # text = file()          #Read its content
+    # print(text)     #Print its contents
+    file.close() 
+else:
+    print("403 Unknown Error Occured :/")
